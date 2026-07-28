193d50692eb32f2293302e027549a4f47f944845
ngocsonhoang1991 <59438363+ngocsonhoang1991@users.noreply.github.com>
2026-07-28 09:01:47 +0700

    Update disable-update.ps1

--- disable-update.ps1
+++ disable-update.ps1
@@ -1,15 +1,7 @@
-Write-Host "Dang vo hieu hoa triet de Windows Update..." -ForegroundColor Yellow
+Stop Service Update Window Server 
 
-# 1. Tat Windows Update Service
-Stop-Service -Name "wuauserv" -Force -ErrorAction SilentlyContinue
-Set-Service -Name "wuauserv" -StartupType Disabled
+Stop-Service wuauserv -Force
+Set-Service wuauserv -StartupType Disabled
 
-# 2. Tat Update Orchestrator Service
-Stop-Service -Name "UsoSvc" -Force -ErrorAction SilentlyContinue
-Set-Service -Name "UsoSvc" -StartupType Disabled
-
-# 3. Tat Windows Update Medic Service (Bac si tu dong bat lai Update)
-Stop-Service -Name "waasmedicsvc" -Force -ErrorAction SilentlyContinue
-Set-Service -Name "waasmedicsvc" -StartupType Disabled
-
-Write-Host "Da vo hieu hoa thanh cong tat ca cac dich vu Windows Update!" -ForegroundColor Green
+Stop-Service UsoSvc -Force -ErrorAction SilentlyContinue
+Set-Service UsoSvc -StartupType Disabled
